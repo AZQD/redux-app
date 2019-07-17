22670e249f21780f5d54bf0ba466cbbbcef0f180
liuchaojie <604972395@qq.com>
2019-07-17 21:33:39 +0800

    更新组件

--- src/component/post/Index.js
+++ src/component/post/Index.js
@@ -1,11 +1,36 @@
 import React, {Component} from 'react';
+// import axios from 'axios';
+import {fetch} from 'whatwg-fetch'
 
 class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      postData: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+      .then(post => {
+        this.setState({
+          postData: post
+        });
+      });
+  }
+
   render() {
+    const { postData } = this.state;
+    const postItem = postData.map(post=>(
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
     return (
       <div>
-        <h3>1234</h3>
-        <p>5432</p>
+        {postItem}
       </div>
     );
   }
