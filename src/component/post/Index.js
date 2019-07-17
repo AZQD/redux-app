51899b91993f99ab33b70cd6dae30814a2573873
liuchaojie <liuchaojie@58ganji.com>
2019-07-17 17:49:21 +0800

    添加post组件

---
+++ src/component/post/Index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        <h3>1234</h3>
+        <p>5432</p>
+      </div>
+    );
+  }
+}
+
+export default Post;
