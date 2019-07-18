73028678d76823d43bc9c4f1563ed3ffe60fe121
liuchaojie <604972395@qq.com>
2019-07-18 13:54:09 +0800

    初始化redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
 import './App.css';
 
 import Post from './component/post/Index.js';
 
+const middleware = [thunk];
+const initState = {};
+const store = createStore(
+  ()=>{},
+  initState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+      <Provider store={store}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
 
-        <Post />
-      </div>
+          <Post />
+        </div>
+      </Provider>
     );
   }
 }
