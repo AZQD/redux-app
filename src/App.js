df9e4ecfeef7a3c5851be8d5f2f00399b46536fa
liuchaojie <604972395@qq.com>
2019-07-18 20:39:15 +0800

    入口格式化

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Provider} from 'react-redux';
 
@@ -14,12 +14,12 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo"/>
             <h2>Welcome to React</h2>
           </div>
 
-          <Add />
-          <Post />
+          <Add/>
+          <Post/>
         </div>
       </Provider>
     );
