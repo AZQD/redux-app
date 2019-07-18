0556571bdc6784df0c305a41777bcfe729bb88ea
liuchaojie <604972395@qq.com>
2019-07-18 20:38:41 +0800

    reducer格式化

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,18 +1,17 @@
-
 const initState = {
   postData: [],
   addData: {}
 };
 
 export default function (state = initState, action) {
-  switch (action.type){
+  switch (action.type) {
     case 'FETCH_POST':
-      return{
+      return {
         ...state,
         postData: action.payload
       };
     case 'ADD_POST':
-      return{
+      return {
         ...state,
         addData: action.payload
       };
